9a4df534c8496be0532947cf8e4443753990edc3
gabryatfendor <sani.gabriele@protonmail.com>
2025-08-12 22:37:11 +0200

    feat: updated unit test to use real gpx, added comment all over the code

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ async fn upload(req_body: String) -> impl Responder {
         Ok(gpx) => {
             let stats = calculate_stats(&gpx);
             HttpResponse::Ok().body(format!(
-                "Total distance: {:.2} km\n\nTotal ascent: {:.2} m\n\nTotal descent: {:.2} m\n\nElevation calculated with threshold of {:.2} meters", 
+                "Track name: {}\n, Total distance: {:.2} km\nTotal ascent: {:.2} m\nTotal descent: {:.2} m\n\nElevation calculated with threshold of {:.2} meters", 
+                stats.track_name,
                 stats.total_distance/1000.0,
                 stats.total_ascent,
                 stats.total_descent,
--- src/stats.rs
+++ src/stats.rs
@@ -3,22 +3,32 @@ use gpx::Gpx;
 const EARTH_RADIUS_IN_METERS: f64 = 6378137.0;
 const ELE_THRESHOLD: f64 = 2.0;
 
+// Struct containing all the stats related to a GpxFile
 pub struct GpxStats {
+    pub track_name: String,
     pub total_distance: f64,
     pub total_ascent: f64,
     pub total_descent: f64,
     pub ele_threshold_used: f64,
 }
 
+impl Default for GpxStats {
+    fn default() -> Self {
+        GpxStats { track_name: String::new(), total_distance: 0.0, total_ascent: 0.0, total_descent: 0.0, ele_threshold_used: ELE_THRESHOLD }
+    }
+}
+
+/// Given a GPX return the complete GpxStats object with all the property
+/// valorized. Uses haversine for distance calculation and to avoid noise in the 
+/// total elevation we set a globale **ELE_THRESHOLD**. The higher the value the more
+/// noise will be cancelled
 pub fn calculate_stats(gpx: &Gpx) -> GpxStats {
-    let mut stats = GpxStats {
-        total_ascent: 0.0,
-        total_descent: 0.0,
-        total_distance: 0.0,
-        ele_threshold_used: 0.0,
-    };
+    let mut stats = GpxStats::default();
 
     if let Some(track) = gpx.tracks.first() {
+        if let Some(track_name) = &track.name {
+            stats.track_name = track_name.clone();
+        }
         let mut prev_ele = 0.0;
         let mut lon1 = 0.0;
         let mut lat1 = 0.0;
@@ -48,10 +58,12 @@ pub fn calculate_stats(gpx: &Gpx) -> GpxStats {
             }
         }
     }
-    stats.ele_threshold_used = ELE_THRESHOLD;
     stats
 }
 
+/// Calculate distance between two points on a sphere using the haversine function
+/// to convert, since the basic pythagorean formula calculate angles differences only.
+/// **EARTH_RADIUS_IN_METERS** value has been taken from google, it should suffice for now
 fn haversine(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> f64 {
     let lat1_rad = lat1.to_radians();
     let lat2_rad = lat2.to_radians();
@@ -67,8 +79,38 @@ fn haversine(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> f64 {
 
 #[cfg(test)]
 mod tests {
+    /// Test calculate_stats using known gpx. The various consts have the correct value rounded to the nearest integer, we can use
+    /// the TEST_PRECISION_BUFFER to decide in which range we want to validate (the lower the number the stricter the test)
+    use super::*;
+    use std::io::BufReader;
+    use std::fs::File;
+    use gpx::{Gpx, read};
     #[test]
     fn test_calculate_stats_simple() {
-        assert!(true);
+        const RIDRACOLI_DISTANCE: f64 = 32148.0;
+        const RIDRACOLI_ASCENT: f64 = 2056.0;
+        const RIDRACOLI_DESCENT: f64 = 2112.0;
+        const TEST_PRECISION_BUFFER: f64 = 1.0;
+        const RIDRACOLI_TRACK_NAME: &str = "Anello Diga di Ridracoli";
+
+        let file = File::open("samples/ridracoli.gpx").unwrap();
+        let reader = BufReader::new(file);
+
+        let gpx: Gpx = read(reader).unwrap();
+        
+        let stats = calculate_stats(&gpx);
+
+        assert_eq!(stats.ele_threshold_used, ELE_THRESHOLD);
+        
+        assert!(stats.total_distance > (RIDRACOLI_DISTANCE - TEST_PRECISION_BUFFER));
+        assert!(stats.total_distance < (RIDRACOLI_DISTANCE + TEST_PRECISION_BUFFER));
+
+        assert!(stats.total_ascent > (RIDRACOLI_ASCENT - TEST_PRECISION_BUFFER));
+        assert!(stats.total_ascent < (RIDRACOLI_ASCENT + TEST_PRECISION_BUFFER));
+
+        assert!(stats.total_descent > (RIDRACOLI_DESCENT - TEST_PRECISION_BUFFER));
+        assert!(stats.total_descent < (RIDRACOLI_DESCENT + TEST_PRECISION_BUFFER));
+
+        assert_eq!(stats.track_name, RIDRACOLI_TRACK_NAME);
     }
 }
