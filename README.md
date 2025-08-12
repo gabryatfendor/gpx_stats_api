2841097fffff1bba21361ddaef6b0148bacb50be
gabryatfendor <sani.gabriele@protonmail.com>
2025-08-12 18:08:18 +0200

    feat: completed basic stats, (very) minimal documentation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gpx_stats_api
 Check for basic stats on any GPX file
+
+## Build
+Simply run **cargo run** on the root directory of the project. This will startup the webserver on localhost:8080, you can reach there the endpoint listed below
+
+## Endpoint List
+
+### /upload \[POST\]
+Accept a GPX file in input and answers with a list of baic stats (ascent, descent, distance)
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,72 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, post};
+use gpx::read;
 use std::io::Cursor;
 
-use gpx::{Gpx, Track, TrackSegment, read, write};
-
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn default() -> impl Responder {
-    HttpResponse::Ok().body("Are you looking for something?")
-}
+const EARTH_RADIUS_IN_METERS: f64 = 6378137.0;
+const ELE_THRESHOLD: f64 = 2.0;
 
 #[post("/upload")]
 async fn upload(req_body: String) -> impl Responder {
     let cursor = Cursor::new(req_body);
     match read(cursor) {
         Ok(gpx) => {
+            let mut total_distance = 0.0;
+            let mut total_ascent = 0.0;
+            let mut total_descent = 0.0;
+
             if let Some(track) = gpx.tracks.first() {
-                println!("Track name: {:?}", track.name);
+                let mut prev_ele = 0.0;
+                let mut lon1 = 0.0;
+                let mut lat1 = 0.0;
                 for segment in &track.segments {
                     for waypoint in &segment.points {
                         let point = waypoint.point();
-                        println!("Latitude: {}, Longitude: {}", point.x(), point.y());
                         if let Some(ele) = waypoint.elevation {
-                            println!("Elevation: {ele}");
-                        } 
+                            if prev_ele > 0.0 {
+                                let difference = ele - prev_ele;
+                                if difference.abs() > ELE_THRESHOLD {
+                                    if difference >= 0.0 {
+                                        total_ascent = total_ascent + (difference)
+                                    } else {
+                                        total_descent = total_descent + (difference.abs())
+                                    }
+                                }
+                            }
+                            prev_ele = ele;
+                        }
+                        let lon2 = point.x();
+                        let lat2 = point.y();
+                        if lon1 > 0.0 && lat1 > 0.0 {
+                            total_distance = total_distance + haversine(lat1, lon1, lat2, lon2);
+                        }
+                        lon1 = lon2;
+                        lat1 = lat2;
                     }
                 }
             }
-            HttpResponse::Ok().body("GPX file succesfully uploaded")
+            HttpResponse::Ok().body(format!("Total distance: {:.2} km\n\nTotal ascent: {:.2} m\n\nTotal descent: {:.2} m\n\nElevation calculated with threshold of {:.2} meters", total_distance/1000.0, total_ascent, total_descent, ELE_THRESHOLD))
         }
-        Err(e) => {
-            HttpResponse::BadRequest().body(format!("Error in GPX reading: {}", e))
-        }
+        Err(e) => HttpResponse::BadRequest().body(format!("Error in GPX reading: {}", e)),
     }
+}
+
+fn haversine(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> f64 {
+    let lat1_rad = lat1.to_radians();
+    let lat2_rad = lat2.to_radians();
+    let delta_lat = (lat2 - lat1).to_radians();
+    let delta_lon = (lon2 - lon1).to_radians();
+
+    let a = (delta_lat / 2.0).sin().powi(2)
+        + lat1_rad.cos() * lat2_rad.cos() * (delta_lon / 2.0).sin().powi(2);
+    let c = 2.0 * a.sqrt().asin();
+
+    EARTH_RADIUS_IN_METERS * c
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(default)
-            .service(upload)
-    }).bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(upload))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
